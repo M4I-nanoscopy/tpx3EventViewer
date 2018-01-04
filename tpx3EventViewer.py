e6ab4d584fcc6ab2be647dc7e25fb504ba2b2b7d
Paul van Schayck <polleke@gmail.com>
2018-01-04 15:10:58 +0100

    Add command line arguments, ability to save tifs easily, and to show hits instead of events

--- tpx3EventViewer.py
+++ tpx3EventViewer.py
@@ -1,22 +1,58 @@
 #!/usr/bin/env python
-
+import argparse
 import sys
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy.sparse
 from matplotlib.widgets import Slider
 from PIL import Image
+import os
 
 
 def main():
-    f = h5py.File(sys.argv[1], 'r')
+    settings = parse_arguments()
 
-    frame = build(f['events'][()])
+    f = h5py.File(settings.FILE, 'r')
 
-    im = Image.fromarray(frame, 'L')
-    #im.save('test.tif')
-    show(frame)
+    # Build frame
+    if settings.hits:
+        source = 'hits'
+    else:
+        source = 'events'
+
+    frame = build(f[source][()])
+
+    # Output
+    if settings.t:
+        im = Image.fromarray(frame, 'L')
+
+        if settings.f:
+            filename = settings.f
+        else:
+            filename = os.path.splitext(settings.FILE)[0] + ".tif"
+
+        im.save(filename)
+
+    if not settings.n:
+        show(frame)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description=__doc__,  # printed with -h/--help
+        # Don't mess with format of description
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+
+    parser.add_argument('FILE', help="Input .h5 file")
+    parser.add_argument("-t", action='store_true', help="Save 8-bit .tif file")
+    parser.add_argument("-f", metavar='FILE', help="File name for .tif file (default is .h5 file with .tif extension)")
+    parser.add_argument("-n", action='store_true', help="Don't show matplotlib frame")
+    parser.add_argument("--hits", action='store_true', help="Use /hits instead of /events")
+
+    settings = parser.parse_args()
+
+    return settings
 
 
 def show(frame):
@@ -67,7 +103,7 @@ def build(events):
 # This function converts the event data to a 256 by 256 matrix and places the chips into a full frame
 # Also deals with the chip positions
 def events_to_frame(frame):
-    img = np.zeros(shape=(512, 512), dtype=np.uint16)
+    img = np.zeros(shape=(512, 512), dtype=np.uint8)
 
     xedges = np.arange(0, 257, 1)
     yedges = np.arange(0, 257, 1)
@@ -81,7 +117,7 @@ def events_to_frame(frame):
 
         hist = np.histogram2d(rows, cols, bins=(xedges, yedges))
         chip_frame = hist[0]
-        #chip_frame = scipy.sparse.coo_matrix((data, (rows, cols)), shape=(256, 256))
+        # chip_frame = scipy.sparse.coo_matrix((data, (rows, cols)), shape=(256, 256))
 
         # if chip == 0:
         #     img[256:512, 256:512] = np.rot90(chip_frame.todense(), k=1)
@@ -101,6 +137,7 @@ def events_to_frame(frame):
             img[0:256, 0:256] = np.rot90(chip_frame, k=-1)
         if chip == 3:
             img[256:512, 0:256] = np.rot90(chip_frame, k=1)
+
         # if chip == 0:
         #     img[512:1024, 512:1024] = np.rot90(chip_frame, k=1)
         # if chip == 1:
